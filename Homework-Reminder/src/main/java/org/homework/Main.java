8fe06923187da4f9a63bf398881d2ca63eb871aa
Chenyi <971984463@qq.com>
2023-11-04 05:12:29 +0800

    Main as Application

--- Homework-Reminder/src/main/java/org/homework/Main.java
+++ Homework-Reminder/src/main/java/org/homework/Main.java
@@ -1,8 +1,14 @@
 package org.homework;
 
 
+import org.homework.controller.WelcomeController;
+import org.homework.ui.WelcomeUI;
+
 public class Main {
     public static void main(String[] args) {
         /*入口*/
+        do {
+            WelcomeUI.show();
+        } while (WelcomeController.isContinue(true));
     }
 }
